8badbe7edfc263c5c9e3da177ffe1deac194974a
zhangyongsheng <864079692@qq.com>
2022-05-09 11:25:49 +0800

    '查询字符串，获取request'

--- project_01/book/urls.py
+++ project_01/book/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from book.views import create_book
-from book.views import shop
+from book.views import create_book,delete_book,shop,school
 
 urlpatterns = {
     path('create/',create_book),
-    path('<city_id>/<shop_id>',shop),
+    # <占位符> 可以是任意的数字和字符组合
+    path('<prince_id>/<city_id>/<school_id>/',school),
+    path('<city_id>/<shop_id>/',shop),
+    path('delete/',delete_book),
 }
--- project_01/book/views.py
+++ project_01/book/views.py
@@ -10,9 +10,29 @@ def create_book(request):
     #     readcount=25,
     # )
     return HttpResponse("create")
+def delete_book(requset):
+    Book.objects.filter(name='python').delete()
+    return  HttpResponse("delete")
 
 def shop(request,city_id,shop_id):
-    print(city_id,shop_id)
-
+    Query_params = request.GET
+    # 查询字符串，字典类型，{key:value},
+    # 但是Query_params可以一Key多value
+    print(Query_params)
+    order = Query_params.getlist('keys')
+    print(order)
     return HttpResponse("分店")
+
+def school(request,prince_id,city_id,school_id):
+    print(prince_id,city_id,school_id)
+
+    return HttpResponse("高校系统")
+
+"""
+查询字符串
+IP：PORT/path/path/?key1=value1 & key2=value2.....
+
+
+
+"""
 
